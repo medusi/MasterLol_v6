396242b039b59b71fc706140265edb4741084f20
medusi <154091016+medusi@users.noreply.github.com>
2023-12-19 22:42:18 +0100

    update file

---
+++ MasterLol/MasterLol.SDK/SDK/colors_define.h
@@ -0,0 +1,60 @@
+#ifndef COLORS_DEFINE_H
+#define COLORS_DEFINE_H
+
+#pragma once
+
+#include <d3d9.h>
+
+#define WHITE_A(alpha)			D3DCOLOR_ARGB(alpha, 255, 255, 255)
+#define WHITE()					D3DCOLOR_ARGB(255, 255, 255, 255)
+
+#define BLACK_A(alpha)			D3DCOLOR_ARGB(alpha, 0, 0, 0)
+#define BLACK()					D3DCOLOR_ARGB(255, 0, 0, 0)
+
+#define RED_A(alpha)				D3DCOLOR_ARGB(alpha, 255, 0, 0)
+#define RED()					D3DCOLOR_ARGB(255, 255, 0, 0)
+
+#define GREEN_A(alpha)			D3DCOLOR_ARGB(alpha, 0, 128, 0)
+#define GREEN()					D3DCOLOR_ARGB(255, 50, 205, 50)
+
+#define LAWNGREEN_A(alpha)		D3DCOLOR_ARGB(alpha, 124, 252, 0)
+#define LAWNGREEN()				D3DCOLOR_ARGB(alpha, 124, 252, 0)
+
+
+#define BLUE_A(alpha)				D3DCOLOR_ARGB(alpha, 0, 200, 255)
+#define BLUE()					D3DCOLOR_ARGB(255, 0, 200, 255)
+
+#define DEEPSKYBLUE_A(alpha)		D3DCOLOR_ARGB(alpha, 0, 191, 255)
+#define DEEPSKYBLUE()			D3DCOLOR_ARGB(255, 0, 191, 255)
+
+#define SKYBLUE_A(alpha)			D3DCOLOR_ARGB(alpha, 0, 122, 204)
+#define SKYBLUE()				D3DCOLOR_ARGB(255, 0, 122, 204)
+
+#define YELLOW_A(alpha)			D3DCOLOR_ARGB(alpha, 255, 255, 0)
+#define YELLOW()				D3DCOLOR_ARGB(255, 255, 255, 0)
+
+#define ORANGE_A(alpha)			D3DCOLOR_ARGB(alpha, 255, 165, 0)
+#define ORANGE()				D3DCOLOR_ARGB(255, 255, 165, 0)
+
+#define DARKORANGE_A(alpha)		D3DCOLOR_ARGB(alpha, 255, 140, 0)
+#define DARKORANGE()			D3DCOLOR_ARGB(255, 255, 140, 0)
+
+#define PURPLE_A(alpha)			D3DCOLOR_ARGB(alpha, 125, 0, 255)
+#define PURPLE()				D3DCOLOR_ARGB(255, 125, 0, 255)
+
+#define CYAN_A(alpha)				D3DCOLOR_ARGB(alpha, 0, 255, 255)
+#define CYAN()					D3DCOLOR_ARGB(255, 0, 255, 255)
+
+#define PINK_A(alpha)				D3DCOLOR_ARGB(alpha, 255, 20, 147)
+#define PINK()					D3DCOLOR_ARGB(255, 255, 20, 147)
+
+#define GRAY_A(alpha)				D3DCOLOR_ARGB(alpha, 128, 128, 128)
+#define GRAY()					D3DCOLOR_ARGB(255, 128, 128, 128)
+
+#define DARKGRAY_A(alpha)			D3DCOLOR_ARGB(alpha, 73, 73, 73)
+#define DARKGRAY()				D3DCOLOR_ARGB(255, 73, 73, 73)
+
+#define DARKERGRAY_A(alpha)		D3DCOLOR_ARGB(alpha, 31, 31, 31)
+#define DARKERGRAY()			D3DCOLOR_ARGB(255, 31, 31, 31)
+
+#endif /* _COLORS_H_ */
